6a70c28f4fedf2d6f8985a53cfe933b0422a2b00
yhappcom <yh.app.com@gmail.com>
2026-09-22 10:29:57 +0900

    candidate17: load deterministic fonts in render harness

--- test/candidate17_render_test.dart
+++ test/candidate17_render_test.dart
@@ -3,6 +3,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
 import '../lib/home_candidate_17.dart';
 
 Future<void> render(WidgetTester tester, Brightness brightness, String name) async {
@@ -13,7 +14,7 @@ Future<void> render(WidgetTester tester, Brightness brightness, String name) asy
   final key = GlobalKey();
   await tester.pumpWidget(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(brightness: brightness, useMaterial3: true),
+    theme: ThemeData(brightness: brightness, useMaterial3: true, fontFamily: 'C17Sans'),
     home: RepaintBoundary(key: key, child: const HomeCandidate17()),
   ));
   await tester.pumpAndSettle();
@@ -33,6 +34,12 @@ Future<void> render(WidgetTester tester, Brightness brightness, String name) asy
 }
 
 void main() {
+  setUpAll(() async {
+    final sans = FontLoader('C17Sans')..addFont(rootBundle.load('candidate17/fonts/Inter.ttf'));
+    final mono = FontLoader('C17Mono')..addFont(rootBundle.load('candidate17/fonts/RobotoMono.ttf'));
+    await sans.load();
+    await mono.load();
+  });
   testWidgets('candidate17 light 390x844', (t) => render(t, Brightness.light, 'candidate17-light-390x844'));
   testWidgets('candidate17 dark 390x844', (t) => render(t, Brightness.dark, 'candidate17-dark-390x844'));
 }
